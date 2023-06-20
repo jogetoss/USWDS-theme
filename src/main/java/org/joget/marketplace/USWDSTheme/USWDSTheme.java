70324a040f6f58ba1b4898d95bf65b9ba6c552dc
Derek Chen <62978237+derekchentc@users.noreply.github.com>
2023-06-20 12:14:44 +0800

    Added: USWDS-theme - Initial Commit for US Web Design Theme Plugin. @8.0-SNAPSHOT

---
+++ src/main/java/org/joget/marketplace/USWDSTheme/USWDSTheme.java
@@ -0,0 +1,72 @@
+package org.joget.marketplace.USWDSTheme;
+
+import java.util.Map;
+import org.joget.apps.app.service.AppUtil;
+import org.joget.apps.userview.lib.AjaxUniversalTheme;
+
+public class USWDSTheme extends AjaxUniversalTheme{
+    @Override
+    public String getName() {
+        return "US Web Design Theme";
+    }
+
+    @Override
+    public String getVersion() {
+        return "8.0.0";
+    }
+
+    @Override
+    public String getLabel() {
+        return getName();
+    }
+    
+    @Override
+    public String getDescription() {
+        return "A US Web Design System Theme based on Material Design";
+    }
+    
+    @Override
+    public String getClassName() {
+        return getClass().getName();
+    }
+    
+    @Override
+    protected String generateLessCss() {
+        String css = super.generateLessCss();
+        String newcss = AppUtil.readPluginResource(getClass().getName(), "resources/USWDSTheme.css", null, true, null);
+
+        newcss += css ;
+        
+        return newcss;
+    }
+    
+    @Override
+    public String getPropertyOptions() {
+        return AppUtil.readPluginResource(getClass().getName(), "/properties/dx8USWDSTheme.json", null, true, null);
+    }
+    
+    @Override
+    public String getHeader(Map<String, Object> data) {
+        if (isAjaxContent(data)) {
+            return "";
+        } else {
+            if ("true".equals(getPropertyString("horizontal_menu"))) {
+                data.put("header_help_before", "<span class=\"grow\"></span>");
+            }
+            return super.getHeader(data);
+        }
+    }
+    @Override
+    public String getJsCssLib(Map<String, Object> data) {
+        if (isAjaxContent(data)) {
+            return "";
+        } else {
+            String jsCssLink = super.getJsCssLib(data);
+            jsCssLink += "<link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\n";
+            jsCssLink += "<link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\n";
+            jsCssLink += "<link href=\"https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap\" rel=\"stylesheet\">";
+
+            return jsCssLink;
+        }
+    }
+}
